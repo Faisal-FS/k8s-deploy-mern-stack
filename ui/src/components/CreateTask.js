a738c9cb45ca00a4216381b731cfd5bcd335fea6
Faisal Shahzad <faisalshahzad.ciit@gmail.com>
2023-01-17 16:15:26 +0500

    Minor changes in UI, Fixes mongo db conn str
    Signed-off-by: Faisal Shahzad <faisalshahzad.ciit@gmail.com>

--- ui/src/components/CreateTask.js
+++ ui/src/components/CreateTask.js
@@ -16,7 +16,7 @@ export default function CreateTask(props) {
         <div className="container">
             <div className="row">
                 <div className="col-md-12 mrgnbtm">
-                <h2>ToDo List</h2>
+                <h2>List</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="row mrgnbtm">
                         <div className="form-group col-md-6">
